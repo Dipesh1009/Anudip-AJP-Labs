305d50ec9086b2727a1ada5f3567e58aec9cfb84
Dipesh <dipesh09sahu2003@gmail.com>
2024-09-05 19:25:24 +0530

    Added comments

--- overloading/over.java
+++ overloading/over.java
@@ -1,29 +1,66 @@
-package overloading;
-class A {
+/**
+ * @code displays the overloading and overriding concepts of Polymorphism.
+ */
 
-    //load 1 function
+package overloading;
+
+/**
+ * class A is the parent class. In which method overloading is displayed.
+ */
+class A {
+    /**
+     * load(int) is first method.
+     * @param num
+     */
     public void load (int num) {
         System.out.println(num + " I am load 1 function\nI do overloading to load 2\n");
     }
-    
-    // load 2 function
+
+    /**
+     * load(int,int) is second method overloading the first load(int) method.
+     * 
+     * @param num
+     * @param num2
+     */    
     public void load (int num, int num2){
         System.out.println(num + num2 + " I am load 2 function\nI do overloading to load 1\n");
     }
 
-    //load 3 function 
+    /**
+     * load(String) is third method overloading the previous load(int) and laod(int,int) 
+     * methods.
+     * 
+     * @param listen
+     */
     public void load (String listen) {
         System.out.println(listen + " are you sure, you are calling me from class A object, because I am property of class A, not B; I am not even over-rided Is it why you see me? :(\n");
     }
 }
 
+
+/**
+ * class B is child class of class A. It is representing both method overloading as well as
+ * method overriding with load() methods.
+ */
 class B extends A{
-    // load 1 function of child class
+    /**
+     * load(int) is first method of class B, which has same signature as load(int) method of
+     * class A, i.e. overridden method.
+     * 
+     * @param num
+     */
     public void load (int num) {
         System.out.println(num + " I am load 1 function in class B, because you called me not my parent\nI do over riding to load 1 function of my parent class A\n");
     }
     
-    // load 2 function of child class
+    /**
+     * load(int,int) is second method of class B, which has same signature as load(int,int)
+     * method of class A, i.e. overridden method.
+     * 
+     * @param num
+     * @param num
+     * 
+     */
     public void load (int num, int num2) {
         System.out.println("I am overloading my house B's load 1 class, but do you notice; I am from class B, not from A, but let me show you super magic");
         super.load(num, num2);
@@ -46,7 +83,7 @@ public class over {
         b.load(1,1);                // calls the second load function of class B
                                             //which will call eventually class A load 2 function
 
-        b.load("My dear Friend");    // This is not B's func but, it had a parent
+        b.load("My dear Friend");    // This is not class B method but, it had a parent
                                             // and it had not been overridden
 
     }
--- stringProgram/Paranthesis.java
+++ stringProgram/Paranthesis.java
@@ -1,6 +1,46 @@
+/**
+ * TASK: To create a program to find the closing paranthesis of the given paranthesis
+ * in the given string. The paranthesis position is givne with the String value.
+ * 
+ */
+
 package stringProgram;
 
+/**
+ * Paranthesis class is the main class which contains main() and 
+ * findClosingParanthesis(String,int) static methods. 
+ */
 public class Paranthesis {
+
+    /**
+     * findClosingParanthesis(String,int) method checks if the given position has
+     * opening paranthesis '('.
+     * 
+     * If '(' is not found on given index, the function terminates returning -1.
+     * 
+     * After this gets checked, function sets the nestParanthesis(int)
+     * and closeIndex(int) as 0.
+     * 
+     * In for loop, it traverse through the string, checking '(' and ')'.
+     * 
+     * If loop finds '(', it increments value of nestParanthesis(int) by 1.
+     * 
+     * If loop finds ')', it checks value of nestParanthesis(int)
+     *      If 0, 
+     *          sets closeIndex(int) as the value of currentIndex(int) = for loop index
+     *          returns the closeIndex(int) and terminates the function.
+     *      else,
+     *          decrements the value of nestParanthesis(int) by 1. 
+     * 
+     * If loop does not find the corresponding closing bracket ')', 
+     *       loop returns -2 indicating "No closing bracket present for concerned opening bracket"
+     * 
+     * @param str       Contains the String sentence which is to be checked for closing bracket
+     * @param opening   Contains the index of the concerned opening bracket
+     * @return          integer value, -1 if the given index (opening(ing)) doesn't have any opening bracket
+     * @return          -2 if the loop doesn't find concerned closing bracket
+     * @return          (int)closeIndex if loop finds the index of concerned closing bracket
+     */
     static int findClosingParanthesis(String str, int opening) {
         if (str.charAt(opening) != '(') {
             return -1;
@@ -28,6 +68,13 @@ public class Paranthesis {
         return -2;
     }  
 
+    /**
+     * This is main() function
+     * Calls the findClosingParanthesis(String,int) and stores the return value.
+     * 
+     * Checks the returned value and prints the corresponding message.
+     * @param args
+     */
     public static void main(String[] args) {
         String str = "Sometimes (when I nest my parenthesis (also called parentheticals) too many times (like this 1234567890(and this))) they get quite confusing.";
 
@@ -45,3 +92,5 @@ public class Paranthesis {
     
     }
 }
+
+// End Of Program
